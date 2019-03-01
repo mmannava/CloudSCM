7adbd4e1d50011e8174efe5a307d574d1193806b
mmannava <manasapriyamvadamannava@yahoo.com>
2019-02-28 21:51:52 -0600

    updates

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', {page:'Dashboard', menuId:'dash'});
+  res.render('index', {page:'Dashboard', menuId:'dash', scm_avg:'193.27', scm_low:'164.43', scm_high:'243.56', scm_std:'2.89', scm_cov:'0.01', cor:'56.29', for_:'34.24'});
 });
 
 module.exports = router;
