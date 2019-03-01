2da5b35e7aef80323f7b594d427ba3e053126c50
mmannava <manasapriyamvadamannava@yahoo.com>
2019-03-01 00:23:27 -0600

    updates

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', {page:'Service Capability Measure (SCM)', menuId:'dash', scm_avg:'193.27', scm_low:'164.43', scm_high:'243.56', scm_std:'2.89', scm_cov:'0.01', cor:'56.29', for_:'34.24', baseline_scm:'193.5'});
+  res.render('index', {page:'Service Capability Measure (SCM)', menuId:'dash', scm_data: ['193.27', '164.43', '243.56', '2.89', '0.01'] , cor:'56.29', for_:'34.24', baseline_scm:'193.5'});
 });
 
 module.exports = router;
